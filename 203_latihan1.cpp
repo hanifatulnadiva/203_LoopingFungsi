ac8d660f4c12a0bb539b61793affd5e5ade8599b
hanifatulnadiva <161436287+hanifatulnadiva@users.noreply.github.com>
2024-03-19 15:00:39 +0700

    menmabahkan looping using while

--- 203_latihan1.cpp
+++ 203_latihan1.cpp
@@ -1,11 +1,31 @@
 #include <iostream>
 using namespace std;
+
 int main()
-{
-    int i;
+{ int i;
+    string nama[5];
 
     for (i=0;i<7;i++)
-    {
-        cout<< 1<<"."<<"diva "<<endl;
+     {
+        cout<< i<<"."<<"diva "<<endl;
     }
+
+    for (i<7;i>7;i--)
+    {
+        cout<< i<<"."<<"diva "<<endl;
+    }
+
+    for (i=0;i<5;i++){
+        cout <<"masukan nama=";
+        cin>> nama[i];
+    }
+
+    cout<<endl;
+    cout<<"======="<<endl;
+    cout<<"kumpulan nama nama"<<endl;
+    
+    for (i=0;i<5;i++){
+       cout << "nama adalah"<<nama[i]<<endl;
+    }
+
 }
